6c341c42fd5c31393b3e48beaeba5cd13fe89304
rhn <rhn@porcupinefactory.org>
2013-01-06 19:52:49 +0100

    new test

---
+++ tests/flow/intertwined_loops.asm
@@ -0,0 +1,23 @@
+// A test for nested loops structurizing
+// Uses x86_64 assembly
+
+// nontrivial splitting, all nodes present
+//     1
+//     /^
+//   2/  ^
+//   ^\  ^ 
+//  ^  \^  
+//  ^  /3
+//   ^/   
+//    4  
+//    | 
+//    5
+00 <test>:
+    0:  00      nop // 0
+    1:  01      nop // 1
+    2:  02      nop // 2
+    3:  03      nop // 3
+    4:  03      jne 1 // 3
+    5:  04      nop // 4
+    6:  04      jne 3 // 4
+    7:  05      ret // 5
