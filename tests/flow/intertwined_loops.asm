93a9ac6377135036badb34c4f496579d4620d59f
rhn <gihu.rhn@porcupinefactory>
2013-05-05 19:23:17 +0200

    added structured asm test result

--- tests/flow/intertwined_loops.asm
+++ tests/flow/intertwined_loops.asm
@@ -21,23 +21,3 @@
     5:  04      nop // 4
     6:  04      jne 3 // 4
     7:  05      ret // 5
-
-
-// Structure (numbers are sections):
-// function test {
-//    0
-//    flow looplike {{
-//        -> #0
-//        #0 {
-//            1
-//        } -> #1
-//        #1 {
-//            2
-//            3
-//        } -> #0, #2
-//        #2 {
-//            4
-//        } -> #1, end
-//    }} looplike
-//    5
-//}
